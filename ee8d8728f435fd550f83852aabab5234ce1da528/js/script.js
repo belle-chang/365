2a77c821e6eeaaa989c7537ae7d66c8b3444ea00
Anabelle <bellech88@gmail.com>
2022-03-11 14:30:43 -0800

    adding html css

---
+++ ee8d8728f435fd550f83852aabab5234ce1da528/js/script.js
@@ -0,0 +1,461 @@
+let compliments = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "happy birthday sammy ❤️ thank you for encouraging me to be the best version of myself",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "merry christmas 🎄 <br> you have been one of the best presents in my life!",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+    "hi",
+    "swag",
+]
+
+console.log('mone')
+
+var now = new Date();
+let birthday = new Date(2022, 01, 14)  
+console.log(now)
+console.log(birthday)
+var start = new Date(now.getFullYear(), 0, 0);
+var diff = birthday - start;
+var oneDay = 1000 * 60 * 60 * 24;
+var day = Math.floor(diff / oneDay);
+console.log(day)
+
+injectCompliment()
+
+function getDayOfTheYear() {
+    var now = new Date();
+    var start = new Date(now.getFullYear(), 0, 0);
+    var diff = now - start;
+    var oneDay = 1000 * 60 * 60 * 24;
+    return Math.floor(diff / oneDay);
+}
+
+function injectCompliment() {
+    let dayOfYear = getDayOfTheYear();
+    let div = document.getElementById('centered');
+    div.innerHTML = compliments[dayOfYear - 1]
+    switch (dayOfYear) {
+        case 72:
+          turnOnFireworks()
+          break;
+        case 359:
+          turnOnXmas()
+          break;
+      }
+}
+
+function turnOnFireworks() {
+    let pyro = document.getElementsByClassName('pyro-off')[0];
+    pyro.className = 'pyro';
+    let before = document.getElementsByClassName('before-off')[0];
+    before.className = 'before'
+    let after = document.getElementsByClassName('after-off')[0];
+    after.className = 'after'
+    document.body.style = `    
+        margin: 0;
+        padding: 0;
+        background: #000;
+        color: white;
+        overflow: hidden; 
+        font-family: 'Cardo', serif;
+    `
+}
+
+function turnOffFireworks() {
+    let pyro = document.getElementsByClassName('pyro')[0];
+    pyro.className = 'pyro-off';
+    let before = document.getElementsByClassName('before')[0];
+    before.className = 'before-off'
+    let after = document.getElementsByClassName('after')[0];
+    after.className = 'after-off'
+    document.body.style = `
+        background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+        background-size: 400% 400%;
+        animation: gradient 15s ease infinite;
+        height: 100vh;
+    `
+}
+
+function turnOnXmas() {
+    let lightrope = document.getElementsByClassName('lightrope-off')[0];
+    lightrope.className = 'lightrope';
+    document.body.style = `    
+        background: #000;
+        color: white;
+    `
+}
+
+function turnOffXmas() {
+    let lightrope = document.getElementsByClassName('lightrope')[0];
+    lightrope.className = 'lightrope-off';
+    document.body.style = `    
+        background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+        background-size: 400% 400%;
+        animation: gradient 15s ease infinite;
+        height: 100vh;
+    `
+}
+// 02/14: 45 bday
+// 03/14: 72 bday
+// 04/24: 143 graduation
+// 11/12: 316
+// 12/25: 359
